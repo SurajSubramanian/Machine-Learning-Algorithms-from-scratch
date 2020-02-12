d60ab5b3aeddea31577609273ee3c414ea5c2da3
Suraj Subramanian <30796920+SurajSubramanian@users.noreply.github.com>
2020-02-13 02:45:48 +0530

    Update SalaryPrediction.py

--- ML-Programs/SimpleLinearRegression/SalaryPrediction.py
+++ ML-Programs/SimpleLinearRegression/SalaryPrediction.py
@@ -1,11 +1,14 @@
+# importing necessary packages
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
+# loading dataset
 X,Y = np.loadtxt("Salary_Data.csv", skiprows=1,unpack=True, delimiter=',')
 plt.plot(X,Y, 'ro')
 
+# splitting dataset into train and test parts
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.50,random_state=0)
 
 
